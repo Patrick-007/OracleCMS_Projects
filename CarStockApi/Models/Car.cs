328a51d52ae272118d40ce21cdf4a77e94af5931
Patrick-007 <leting.zhouli@student.unimelb.edu.au>
2024-08-27 13:51:45 +1000

    carstockapi

---
+++ CarStockApi/Models/Car.cs
@@ -0,0 +1,18 @@
+namespace CarStockApi.Models
+{
+    public class Car
+    {
+        public int Car_ID {get; set;} // To differentiate between cars
+        public string Make {get; set;}= string.Empty;
+        public string Model {get; set;}= string.Empty;
+        public int Year {get; set;}
+    }
+
+    public class StockItem
+    {
+        public string Make { get; set; }= string.Empty;
+        public string Model { get; set; }= string.Empty;
+        public int Year { get; set; }
+        public int StockLevel { get; set; }
+    }
+}
